8513b9f9ba1ce6bc617895b6dcd7e3dc9c083711
vademich <goncharov1999vadim@gmail.com>
2024-07-25 10:16:29 +0300

    правки по блоку велотур

--- src/app/visual-blocks/about/about.component.ts
+++ src/app/visual-blocks/about/about.component.ts
@@ -17,6 +17,32 @@ import { GreetingsComponent } from '../../components/dialogs/greetings/greetings
     styleUrl: './about.component.less'
 })
 export class AboutComponent {
+    mainBlockTitle = {
+        green: `ПУТЕШЕСТВУЙ`,
+        black: ` С НАМИ!`
+    }
+    mainBlockText =
+        `Велосипед поможет Вам найти новые границы свободы
+        и увидеть этот мир совершенно другими глазами.
+        Начни свое путешествие прямо сейчас.`;
+
+    velBlockTitle = `Велосипеды`;
+    velBlockText =
+        `У нас самый большой парк велосипедов
+        и мы подберем для Вас самый лучший
+        и подходящий велосипед. Ваш
+        велодруг уже ждет Вас и хочет
+        отправится вместе с Вами
+        в прекрасное велопутешесвтие.`;
+
+    tracksBlockTitle = `Велотур`;
+    tracksBlockText =
+        `Наши Велогиды откроют для Вас 4
+        прекрасных маршрута по Черноземью.
+        Это одни из лучших локаций нашего
+        региона. Путешествуй с нами,
+        путешествуй без границ.`;
+
     readonly dialog = inject(MatDialog);
 
     openDialog(): void {

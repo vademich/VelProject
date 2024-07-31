26165aa10f8dd0bd90ec86d9effc2987cfe82deb
vademich <goncharov1999vadim@gmail.com>
2024-07-31 11:35:06 +0300

    ширина окна прайслиста

--- src/app/components/dialogs/price-list/price-list.component.ts
+++ src/app/components/dialogs/price-list/price-list.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
     selector: 'app-price-list',
     standalone: true,
     imports: [
-        CommonModule
+        CommonModule,
+        MatCardModule
     ],
     templateUrl: './price-list.component.html',
     styleUrl: './price-list.component.less'
